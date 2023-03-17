2f443fba8c131673c65aa14c358dbb715fbc2b23
mitchelinoue <mitchel_inoue@msn.com>
2023-03-17 14:35:25 -0300

    aulas

---
+++ Java/src/main/java/Literal.java
@@ -0,0 +1,13 @@
+public class Literal {
+
+    public static void main(String[] args) {
+        int numero;
+        String cadeiaDeCaracteres;
+        char caracter;
+        boolean certoOuErrado;
+
+        numero = 10;
+        cadeiaDeCaracteres = "Meu curdo de Java Fundamentos";
+
+    }
+}
---
+++ Java/src/main/java/TipoDado.java
@@ -0,0 +1,18 @@
+public class TipoDado {
+
+    public static void main(String[] args) {
+        //demonstração de algund tipos de dados
+        int numero;
+        float decimalCurto;
+        double decimalLongo;
+        char caracter;
+        String cadeiaDeCaracter;
+
+        numero = 10;
+        decimalCurto = 10.5f;
+        decimalLongo = 20.5;
+        caracter = 'A';
+        cadeiaDeCaracter = "** Meu curso de Java Fundamentos em 2021 **";
+
+    }
+}
